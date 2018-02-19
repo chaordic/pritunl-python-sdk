fc20e8480dfc5713cbbc30fdfe6dc5bf390c3b16
Raphael P. Ribeiro <raphaelpr01@gmail.com>
2018-02-19 12:52:07 -0300

    put auth vars on the right scope

--- pritunlsdk/auth.py
+++ pritunlsdk/auth.py
@@ -5,28 +5,27 @@ import hmac
 import hashlib
 import base64
 import requests
+import json
 import logging
 
 LOGGER = logging.getLogger(__name__)
 
-API_TOKEN = os.getenv('PRITUNL_API_TOKEN')
-API_SECRET = os.getenv('PRITUNL_API_SECRET')
-BASE_URL = os.getenv('PRITUNL_API_URL')
-
 
 # Taken from https://pritunl.com/api
 def pritunl_auth_request(method, path, headers=None, data=None):
+
+    api_token = os.getenv('PRITUNL_API_TOKEN')
+    api_secret = os.getenv('PRITUNL_API_SECRET')
+    base_url = os.getenv('PRITUNL_API_URL')
+
     auth_timestamp = str(int(time.time()))
     auth_nonce = uuid.uuid4().hex
-    auth_string = '&'.join([
-        API_TOKEN,
-        auth_timestamp,
-        auth_nonce,
+    auth_string = '&'.join([api_token, auth_timestamp, auth_nonce,
         method.upper(), path])
     auth_signature = base64.b64encode(hmac.new(
-        API_SECRET, auth_string, hashlib.sha256).digest())
+        api_secret, auth_string, hashlib.sha256).digest())
     auth_headers = {
-        'Auth-Token': API_TOKEN,
+        'Auth-Token': api_token,
         'Auth-Timestamp': auth_timestamp,
         'Auth-Nonce': auth_nonce,
         'Auth-Signature': auth_signature,
@@ -35,9 +34,9 @@ def pritunl_auth_request(method, path, headers=None, data=None):
         auth_headers.update(headers)
     try:
         return getattr(requests, method.lower())(
-            BASE_URL + path,
+            base_url + path,
             headers=auth_headers,
             data=data,
         )
     except Exception as e:
-        LOGGER.debug('Could not connect to %s: %s' % (BASE_URL, e))
+        LOGGER.debug('Could not connect to %s: %s' % (base_url, e))
